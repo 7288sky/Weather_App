ac0d6af6eb998624455558b8b40a0dd70af04b50
7288sky <123364227+7288sky@users.noreply.github.com>
2023-08-18 23:37:12 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f0a2cfbfc7f8b9ced57fe25dad1abf88",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
